caeaf65d5362956cd4c1f4fbbf68e0726653eb1f
Orcun Yurtseven <yurtsevenorcun@gmail.com>
2022-10-27 17:55:41 +0300

    Final Project

---
+++ OrcunBlog/Controllers/AdminUyeController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using OrcunBlog.Models;
+
+namespace OrcunBlog.Controllers
+{
+    public class AdminUyeController : Controller
+    {
+        private BloggerEntities db = new BloggerEntities();
+
+        // GET: AdminUye
+        public ActionResult Index()
+        {
+            return View(db.Uyes.ToList());
+        }
+
+        // GET: AdminUye/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Uye uye = db.Uyes.Find(id);
+            if (uye == null)
+            {
+                return HttpNotFound();
+            }
+            return View(uye);
+        }
+
+        // GET: AdminUye/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AdminUye/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "UyeID,UyeAdSoyad,UyeMail,UyeSifre")] Uye uye)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Uyes.Add(uye);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(uye);
+        }
+
+        // GET: AdminUye/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Uye uye = db.Uyes.Find(id);
+            if (uye == null)
+            {
+                return HttpNotFound();
+            }
+            return View(uye);
+        }
+
+        // POST: AdminUye/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "UyeID,UyeAdSoyad,UyeMail,UyeSifre")] Uye uye)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(uye).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(uye);
+        }
+
+        // GET: AdminUye/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Uye uye = db.Uyes.Find(id);
+            if (uye == null)
+            {
+                return HttpNotFound();
+            }
+            return View(uye);
+        }
+
+        // POST: AdminUye/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Uye uye = db.Uyes.Find(id);
+            db.Uyes.Remove(uye);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Uye uye)
+        {
+            var login = db.Uyes.Where(u => u.UyeMail == uye.UyeMail && u.UyeSifre == uye.UyeSifre).FirstOrDefault();
+            if (login!=null)
+            {
+                Session["uyeID"] = login.UyeID;
+                return RedirectToAction("Index", "AdminBlog");
+            }
+            else
+            {
+                ViewBag.HataliGiris = "Hatalı Giriş Denemesi Yaptınız tekrar deneyiniz";
+            }
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            Session["UyeID"] = null;
+            Session.Abandon();
+            return RedirectToAction("Index", "Blog");
+        }
+    }
+}
---
+++ OrcunBlog/Controllers/BlogController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OrcunBlog.Models;
+using PagedList;
+using PagedList.Mvc;
+
+
+namespace OrcunBlog.Controllers
+{
+    public class BlogController : Controller
+    {
+        BloggerEntities db = new BloggerEntities();
+        // GET: Blog
+        public ActionResult Index(int page = 1)
+        {
+            var blog = db.Blogs.OrderByDescending(b => b.BlogID).ToPagedList(page, 3);
+            return View(blog);
+        }
+
+        public ActionResult Hakkinda()
+        {
+            var hakkinda = db.Hakkindas.ToList();
+            return View(hakkinda);
+        }
+
+        public ActionResult BlogDetay(int id)
+        {
+            var blogdetay = db.Blogs.Where(b => b.BlogID == id).SingleOrDefault();
+            if (blogdetay == null)
+            {
+                return HttpNotFound();
+
+            }
+            return View(blogdetay);
+        }
+
+        public ActionResult OkunmaArttir(int blogid)
+        {
+            var blog = db.Blogs.Where(b => b.BlogID == blogid).SingleOrDefault();
+            blog.BlogOkunmaSayisi = blog.BlogOkunmaSayisi + 1;
+            db.SaveChanges();
+            return View();
+        }
+
+    }
+}
